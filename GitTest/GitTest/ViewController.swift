b31d3750b571d02f9b99b758be5dd4cea01e5b6d
Niharika R Kadamba <niharika.kadamba@robosoftin.com>
2017-11-23 10:07:19 +0530

    Committing project file created

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Niharika R Kadamba on 23/11/17.
+//  Copyright © 2017 Niharika R Kadamba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
